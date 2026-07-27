da0185776c2b706f222c96aaa16d9d54975d03ba
DINESH <157237445+DineshWebDev2003@users.noreply.github.com>
2026-07-27 19:13:40 +0530

    Fix: use bash for key.properties (env vars), Python only for build.gradle patch

--- my-expo-app/scripts/configure-signing.sh
+++ my-expo-app/scripts/configure-signing.sh
@@ -2,45 +2,35 @@
 set -e
 
 echo "$CM_KEYSTORE" > /tmp/keystore.b64
+python3 -c "import base64; open('/tmp/keystore.keystore','wb').write(base64.b64decode(open('/tmp/keystore.b64').read()))"
 
-python3 << 'PYEOF'
-import base64, os
+cat > android/key.properties << EOF
+storeFile=/tmp/keystore.keystore
+storePassword=$CM_KEYSTORE_PASSWORD
+keyAlias=$CM_KEY_ALIAS
+keyPassword=$CM_KEY_PASSWORD
+EOF
 
-with open('/tmp/keystore.b64') as f:
-    ks = f.read()
-with open('/tmp/keystore.keystore', 'wb') as f:
-    f.write(base64.b64decode(ks))
-
-with open('android/key.properties', 'w') as f:
-    f.write('storeFile=/tmp/keystore.keystore\n')
-    f.write('storePassword=' + os.environ['CM_KEYSTORE_PASSWORD'] + '\n')
-    f.write('keyAlias=' + os.environ['CM_KEY_ALIAS'] + '\n')
-    f.write('keyPassword=' + os.environ['CM_KEY_PASSWORD'] + '\n')
-
-with open('android/app/build.gradle', 'r') as f:
+python3 -c "
+with open('android/app/build.gradle','r') as f:
     c = f.read()
-
-header = '''
+h = '''
 def keystorePropertiesFile = rootProject.file('key.properties')
 def keystoreProperties = new Properties()
 if (keystorePropertiesFile.exists()) {
     keystoreProperties.load(new FileInputStream(keystorePropertiesFile))
 }
 '''
-c = c.replace('def projectRoot = rootDir', header + 'def projectRoot = rootDir')
-
-release_signing = '''        release {
+c = c.replace('def projectRoot = rootDir', h + 'def projectRoot = rootDir')
+b = '''        release {
             storeFile file(keystoreProperties['storeFile'])
             storePassword keystoreProperties['storePassword']
             keyAlias keystoreProperties['keyAlias']
             keyPassword keystoreProperties['keyPassword']
         }
 '''
-c = c.replace('signingConfigs {\n        debug {', 'signingConfigs {\n' + release_signing + '        debug {')
-c = c.replace('signingConfig signingConfigs.debug', 'signingConfig signingConfigs.release', 1)
-
-with open('android/app/build.gradle', 'w') as f:
-    f.write(c)
-
-print('Signing configured successfully')
-PYEOF
+c = c.replace('signingConfigs {\n        debug {','signingConfigs {\n' + b + '        debug {')
+c = c.replace('signingConfig signingConfigs.debug','signingConfig signingConfigs.release',1)
+open('android/app/build.gradle','w').write(c)
+print('Signing configured')
+"
