0885d67b4b316911abb86558a79d46de9fcd4feb
DINESH <157237445+DineshWebDev2003@users.noreply.github.com>
2026-07-27 19:07:33 +0530

    Fix signing config to use key.properties file instead of env vars

--- my-expo-app/scripts/configure-signing.sh
+++ my-expo-app/scripts/configure-signing.sh
@@ -1,25 +1,46 @@
 #!/bin/bash
 set -e
-export KEYSTORE_PATH=/tmp/keystore.keystore
-export KEYSTORE_PASSWORD=$CM_KEYSTORE_PASSWORD
-export KEY_ALIAS=$CM_KEY_ALIAS
-export KEY_PASSWORD=$CM_KEY_PASSWORD
 
 echo "$CM_KEYSTORE" > /tmp/keystore.b64
-python3 -c "import base64; open('/tmp/keystore.keystore','wb').write(base64.b64decode(open('/tmp/keystore.b64').read()))"
 
-python3 -c "
-with open('android/app/build.gradle','r') as f:
+python3 << 'PYEOF'
+import base64, os
+
+with open('/tmp/keystore.b64') as f:
+    ks = f.read()
+with open('/tmp/keystore.keystore', 'wb') as f:
+    f.write(base64.b64decode(ks))
+
+with open('android/key.properties', 'w') as f:
+    f.write('storeFile=/tmp/keystore.keystore\n')
+    f.write('storePassword=' + os.environ['CM_KEYSTORE_PASSWORD'] + '\n')
+    f.write('keyAlias=' + os.environ['CM_KEY_ALIAS'] + '\n')
+    f.write('keyPassword=' + os.environ['CM_KEY_PASSWORD'] + '\n')
+
+with open('android/app/build.gradle', 'r') as f:
     c = f.read()
-b = '''        release {
-            storeFile file(System.getenv('KEYSTORE_PATH'))
-            storePassword System.getenv('KEYSTORE_PASSWORD')
-            keyAlias System.getenv('KEY_ALIAS')
-            keyPassword System.getenv('KEY_PASSWORD')
+
+header = '''
+def keystorePropertiesFile = rootProject.file('key.properties')
+def keystoreProperties = new Properties()
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(new FileInputStream(keystorePropertiesFile))
+}
+'''
+c = c.replace('def projectRoot = rootDir', header + 'def projectRoot = rootDir')
+
+release_signing = '''        release {
+            storeFile file(keystoreProperties['storeFile'])
+            storePassword keystoreProperties['storePassword']
+            keyAlias keystoreProperties['keyAlias']
+            keyPassword keystoreProperties['keyPassword']
         }
 '''
-c = c.replace('signingConfigs {\n        debug {','signingConfigs {\n' + b + '        debug {')
-c = c.replace('signingConfig signingConfigs.debug','signingConfig signingConfigs.release',1)
-open('android/app/build.gradle','w').write(c)
-print('Signing configured')
-"
+c = c.replace('signingConfigs {\n        debug {', 'signingConfigs {\n' + release_signing + '        debug {')
+c = c.replace('signingConfig signingConfigs.debug', 'signingConfig signingConfigs.release', 1)
+
+with open('android/app/build.gradle', 'w') as f:
+    f.write(c)
+
+print('Signing configured successfully')
+PYEOF
